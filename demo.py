a8c7653e9783f7afb6ef08fac84fbd1bb9037292
Jair <1096224896@qq.com>
2023-10-14 15:14:12 +0800

    df

--- demo.py
+++ demo.py
@@ -1,8 +1,3 @@
 import requests
 from lxml import etree
 
-url = 'http://www.baidu.com'
-response = requests.get(url)
-html = etree.HTML(response.text)
-result = etree.tostring(html)
-print(result.decode('utf-8'))
